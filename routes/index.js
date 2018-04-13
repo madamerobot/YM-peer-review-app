5cec5775021ff9f3eb4ec71bcad32fd8c522f97a
Valerie Fuchse <valerie@yourmajesty.co>
2018-04-13 14:59:28 +0200

    Set up routes for initial views, build template nunjucks file which gets extended

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', async(ctx) => {
     await ctx.render('home')
 })
 
+router.get('/register', async(ctx) => {
+    await ctx.render('registration')
+})
+
+router.get('/users', async(ctx) => {
+    await ctx.render('user-overview')
+})
+
+router.get('/user/:username', async(ctx) => {
+    await ctx.render('user-detail')
+})
+
+router.get('/conflicts', async(ctx) => {
+    await ctx.render('conflict-overview')
+})
+
+router.get('/conflicts/:username', async(ctx) => {
+    await ctx.render('conflict-detail')
+})
+
+router.get('/underallocated', async(ctx) => {
+    await ctx.render('underallocated-overview')
+})
+
 module.exports = router
